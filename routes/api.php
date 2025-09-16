76cd8be51842a07aefedf2f1d4e44dd958683421
fernandoreycaires <fernando.rey.caires@gmail.com>
2025-09-16 11:05:23 -0300

    README

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::post('/v1/register', [AuthController::class, 'register']);
 Route::post('/v1/login', [AuthController::class, 'login']);
 
+## Autenticados
 Route::middleware('auth:api')->group(function () {
-    Route::get('/v1/user', [AuthController::class, 'user']);
-    Route::post('/v1/logout', [AuthController::class, 'logout']);
-    Route::get('/v1/users', [AuthController::class, 'users']);
+    Route::get('/v1/user', [AuthController::class, 'user']); ## Mostra usuario logado
+    Route::post('/v1/logout', [AuthController::class, 'logout']);  ## Invalida token fazendo com que usuario deslogue
+    Route::get('/v1/users', [AuthController::class, 'users']);  ## Lista todos os usuarios 
 });
